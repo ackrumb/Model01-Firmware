e6556127636eae9bbbdef829c21ffdd64b21e199
Ari Krumbein <arkrumbe@microsoft.com>
2019-02-13 10:19:49 -0800

    moved das h to butterfly key

--- Model01-FirmwareMod/Model01-FirmwareMod.ino
+++ Model01-FirmwareMod/Model01-FirmwareMod.ino
@@ -212,7 +212,7 @@ KEYMAPS(
    M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
    Key_Enter,     Key_J, Key_L, Key_U,     Key_Y,         Key_Semicolon, Key_Equals,
                   Key_H, Key_N, Key_E,     Key_I,         Key_O,         Key_Quote,
-   Key_Period,  Key_K, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
+   Key_Minus,  Key_K, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
    Key_LeftGui, Key_RightShift, Key_Backspace, Key_RightControl,
    ShiftToLayer(FUNCTION)),
 
